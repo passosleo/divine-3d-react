4ecc33664f14c597e474532cf34df498ef242db1
Aluno Faccat <aluno@lab203-08.LABORATORIO>
2022-06-30 20:31:00 -0300

    feat: routes

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -1,11 +1,27 @@
-import React from 'react';
+
+import React from "react";
+import { BrowserRouter, Routes as Switch, Route } from "react-router-dom";
+import Layout from "../layout";
+import Home  from "../pages/Home";
+import Login from "../pages/Login";
+import Register from "../pages/Register";
+import Product from "../pages/Product";
+import Checkout from "../pages/Checkout";
 
 const Routes = () => {
-	return (
-		<div>
-			<h1>Hello</h1>
-		</div>
-	)
-}
+  return (
+    <BrowserRouter>
+      <Layout>
+        <Switch>
+          <Route path="/" element={<Home />} />
+					<Route path="login" element={<Login />} />
+					<Route path="cadastro" element={<Register />} />
+					<Route path="produto" element={<Product />} />
+					<Route path="Checkout" element={<Checkout />} />
+        </Switch>
+      </Layout>
+    </BrowserRouter>
+  );
+};
 
 export default Routes;
