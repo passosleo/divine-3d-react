31dc4d69d8f0d4f6b766544143ffed3e007aa1c4
rayanemelo <raymeloc@gmail.com>
2022-06-30 20:17:35 -0300

    feat: pages

---
+++ src/pages/Checkout.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Checkout = () => {
+    return(
+        <div>oi</div>
+    );
+};
+
+export default Checkout;
---
+++ src/pages/Product.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Product = () => {
+    return(
+        <div>oi</div>
+    );
+};
+
+export default Product;
