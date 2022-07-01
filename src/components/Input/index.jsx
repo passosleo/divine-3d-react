591cd04ed6dcc922501c3b5b38ceaf7c2cd2c019
rayanemelo <raymeloc@gmail.com>
2022-06-30 21:56:40 -0300

    finish: cadastro

--- src/components/Input/index.jsx
+++ src/components/Input/index.jsx
@@ -1,14 +1,10 @@
 import {Container, Label, Input} from './styles.js'
 
-export const InputComponent = () => {
-
-    const numbers = [1, 2 ,3,4, 5, 6, 7, 8]
-
+export const InputComponent = ({number, placeholder}) => {
     return(
         <Container>
-            {}
-            <Label>1</Label>
-            <Input placeholder="Nome Completo"/>
+            <Label>{number}</Label>
+            <Input placeholder={placeholder}/>
         </Container>
        
     )
