18c1de2eee7de33bdc0bfef1c6a9c52b4ee35f4c
Aluno Faccat <aluno@lab203-08.LABORATORIO>
2022-06-30 22:01:56 -0300

    feat: header desktop

--- src/layout/index.jsx
+++ src/layout/index.jsx
@@ -1,19 +1,32 @@
 import React from 'react'
 import { Link } from "react-router-dom";
-import { Header } from './styles';
+import { Divider, Header, HeaderContent, Logo, SearchInput, UserActions } from './styles';
+import { FaShoppingCart, FaHeart } from "react-icons/fa";
+import logo from "../assets/images/logo.png";
+import avatar from "../assets/images/avatar.png";
 
 const Layout = ({ children }) => {
 	return (
-    <div>
-      <Header>
-        <Link to="/">Home</Link>
-				<Link to="login">Login</Link>
-				<Link to="cadastro">Cadastro</Link>
-        <Link to="produto">Produto</Link>
-				<Link to="checkout">Checkout</Link>
-      </Header>
+		<>
+			<Header>
+				<HeaderContent>
+					<Link to="/">
+						<Logo src={logo}>
+							<img src={logo} alt="Logo Divine 3D" />
+							<h1>Divine 3D</h1>
+						</Logo>
+					</Link>
+					<SearchInput placeholder="Procurar..." />
+					<UserActions>
+						<FaShoppingCart color="#F5F5F5" size="1.3em" style={{margin: "0 10px", cursor: "pointer"}}/>
+						<FaHeart color="#F5F5F5" size="1.3em" style={{margin: "0 10px", cursor: "pointer"}}/>
+						<Divider />
+						<img src={avatar} alt="Logo Divine 3D"/>
+					</UserActions>
+				</HeaderContent>
+			</Header>
 			{children}
-    </div>
+		</>
 	);
 };
 
--- src/layout/styles.js
+++ src/layout/styles.js
@@ -1,10 +1,70 @@
 import styled from 'styled-components'
 
 const bg = "#950101"
+const white = "#F5F5F5"
 
 export const Header = styled.div`
-    display:flex;
-    heigth: 80px;
-		width: 100vw;
-		background-color: ${bg};
+	display: flex;
+	align-items: center;
+	justify-content: center;
+	height: 60px;
+	width: 100vw;
+	background-color: ${bg};
+	box-shadow: rgba(0, 0, 0, 0.24) 0px 3px 8px;
+`;
+
+export const HeaderContent = styled.div`
+	display: flex;
+	align-items: center;
+	justify-content: space-between;
+	max-width: 1400px;
+	width: 100%;
+`;
+
+export const Logo = styled.div`
+	display: flex;
+	align-items: center;
+	color: ${white};
+
+	img {
+		width: 50px;
+	}
+
+	h1 {
+		font-family: Raleway, sans-serif;
+		font-size: 22px;
+		font-weight: 600;
+		margin-left: 10px;
+	}
+`;
+
+export const SearchInput = styled.input`
+	max-width: 400px;
+	width: 100%;
+	border: none;
+	outline: none;
+	padding: 0 10px;
+	border-radius: 6px;
+	height: 28px;
+`;
+
+export const UserActions = styled.div`
+	display: flex;
+	align-items: center;
+
+	img {
+		width: 40px;
+		height: 40px;
+		border-radius: 100%;
+		margin-left: 10px;
+		cursor: pointer;
+	}
+`;
+
+export const Divider = styled.div`
+	width: 1px;
+	height: 30px;
+	margin: 0 15px;
+	border-radius: 2px;
+	background-color: ${white};
 `;
