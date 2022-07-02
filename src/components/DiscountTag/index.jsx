0006a04c5008bcaa2ab28b37c035c86c8518b023
passosleo <leonardopassos98@gmail.com>
2022-07-01 22:26:47 -0300

    chore(product): product pricing container

---
+++ src/components/DiscountTag/index.jsx
@@ -0,0 +1,5 @@
+import { Tag } from "./styles";
+
+export const DiscountTagComponent = ({ ammount }) => {
+  return <Tag>{ammount}% OFF</Tag>;
+};
--- src/pages/Product/Product.jsx
+++ src/pages/Product/Product.jsx
@@ -1,59 +1,67 @@
 import React from "react";
 import {
+  BoldText,
+  ColorBox,
+  Colors,
+  Conditions,
   Container,
-  PricingContainer,
+  OldPrice,
+  OldPriceContainer,
+  Price,
   ProductContainer,
   ProductImage,
-  ProductRating,
   ProductTitle,
-  RatingText,
+  SpacedContainer,
 } from "./styles";
 import product from "../../assets/images/vasos/product.jpg";
 import { FaStar, FaHeart } from "react-icons/fa";
+import { DiscountTagComponent } from "../../components/DiscountTag";
+
+const stars = [...Array(5).keys()].map((i) => i + 1);
+const colors = ["#1A4D2E", "#F15412", "#1363DF", "#F32424", "#FF7396"];
 
 const Product = () => {
   return (
     <Container>
+      <ProductImage src={product} />
       <ProductContainer>
-        <ProductImage src={product} />
-        <PricingContainer>
-          <ProductTitle>Vaso de Plantas Rob Plant Sentado</ProductTitle>
-          <ProductRating>
+        <ProductTitle>Vaso de Plantas Rob Plant Sentado</ProductTitle>
+        <SpacedContainer>
+          <div>
+            {stars.map((i) => (
+              <FaStar
+                key={i}
+                color="#950101"
+                size="1.1em"
+                style={{ marginRight: 4, cursor: "pointer" }}
+              />
+            ))}
+            <BoldText>22 avaliações</BoldText>
+          </div>
+          <FaHeart color="#950101" size="1.3em" style={{ cursor: "pointer" }} />
+        </SpacedContainer>
+        <OldPriceContainer>
+          <DiscountTagComponent ammount={29} />
+          <OldPrice>R$69,90</OldPrice>
+        </OldPriceContainer>
+        <SpacedContainer>
+          <Colors>
+            <BoldText>Cores:</BoldText>
             <div>
-              <FaStar
-                color="#950101"
-                size="1.1em"
-                style={{ marginRight: 4, cursor: "pointer" }}
-              />
-              <FaStar
-                color="#950101"
-                size="1.1em"
-                style={{ marginRight: 4, cursor: "pointer" }}
-              />
-              <FaStar
-                color="#950101"
-                size="1.1em"
-                style={{ marginRight: 4, cursor: "pointer" }}
-              />
-              <FaStar
-                color="#950101"
-                size="1.1em"
-                style={{ marginRight: 4, cursor: "pointer" }}
-              />
-              <FaStar
-                color="#D9D9D9"
-                size="1.1em"
-                style={{ marginRight: 4, cursor: "pointer" }}
-              />
-              <RatingText>22 avaliações</RatingText>
+              {colors.map((color, index) => (
+                <ColorBox key={index} color={color} />
+              ))}
             </div>
-            <FaHeart
-              color="#950101"
-              size="1.3em"
-              style={{ marginRight: 4, cursor: "pointer" }}
-            />
-          </ProductRating>
-        </PricingContainer>
+          </Colors>
+          <div>
+            <Price>R$49,90</Price>
+            <Conditions>
+              à vista
+              <br />
+              ou 3x de <strong>R$18,90</strong> no cartão
+            </Conditions>
+          </div>
+        </SpacedContainer>
       </ProductContainer>
     </Container>
   );
