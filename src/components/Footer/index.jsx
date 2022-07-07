3f9928f0232cfa129c880bf88bc24c08ab0f9f5b
rayanemelo <rayanemeloc@lighthouseit.com.br>
2022-07-07 10:19:21 -0300

    footer

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -1,5 +1,46 @@
-import { Footer } from "./styles.js";
-
+import { Footer, Row, RedesSociais } from "./styles.js";
+import {
+  FaWhatsapp,
+  FaEnvelope,
+  FaMapMarkerAlt,
+  FaFacebookF,
+  FaInstagram,
+  FaTwitter,
+} from "react-icons/fa";
 export const FooterComponent = () => {
-  return <Footer />;
+  return (
+    <Footer>
+      <p>Fale conosco</p>
+      <Row>
+        <FaWhatsapp size="1em" />
+        <p>51 99999-9999</p>
+      </Row>
+      <Row>
+        <FaEnvelope size="1em" />
+        <p>email@email.com</p>
+      </Row>
+      <Row>
+        <FaMapMarkerAlt size="1em" />
+        <p>Av. Sebastião Amoretti, 2911 - Centro, Taquara - RS, 95600-000</p>
+      </Row>
+      <RedesSociais>
+        <div>
+          <FaFacebookF size="1em" color="#fff" />
+        </div>
+        <div>
+          <FaInstagram size="1em" color="#fff" />
+        </div>
+        <div>
+          <FaTwitter size="1em" color="#fff" />
+        </div>
+      </RedesSociais>
+      <p>
+        Todas as imagens e produtos foram retirados do site com a sua devida
+        autorização
+      </p>
+      <p>
+        <strong>https://www.vegras.com</strong>
+      </p>
+    </Footer>
+  );
 };
--- src/components/Footer/styles.js
+++ src/components/Footer/styles.js
@@ -1,7 +1,40 @@
-import styled from 'styled-components'
-
+import styled from "styled-components";
 
 export const Footer = styled.div`
-    height: 250px;  
-    background:#F5F5F5; 
-`
+  height: 250px;
+  background: #f5f5f5;
+  padding: 40px 100px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+
+  p:first-child {
+    font-size: 20px;
+    font-weight: bold;
+    margin-bottom: 5px;
+  }
+`;
+
+export const Row = styled.div`
+  display: flex;
+  align-items: center;
+  margin-bottom: 5px;
+  p {
+    margin-left: 5px;
+  }
+`;
+
+export const RedesSociais = styled.div`
+  display: flex;
+
+  div {
+    width: 35px;
+    height: 35px;
+    background: #950101;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    border-radius: 50%;
+    margin: 10px;
+  }
+`;
